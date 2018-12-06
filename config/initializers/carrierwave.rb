cbce6b4fb9b77d6b7c666f182cc6611eb5a05b2c
Hendra Nicholas <hendra@41studio.com>
2018-12-06 18:13:27 +0700

    setup production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,6 @@
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.fog_credentials = {
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV["S3_KEY"],
-      :aws_secret_access_key => ENV["S3_SECRET"]
-    }
-    config.fog_directory    = ENV["S3_BUCKET_NAME"]
-  end
+  config.storage = :file
+  config.permissions = 0666
+  config.directory_permissions = 0777
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
 end

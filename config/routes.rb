e5a07697a650055911ef648b08102ff46f8b6da0
Iman Ismayana <iman.ismayana@41studio.com>
2018-12-07 11:37:31 +0700

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
 
   ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
-  devise_for :users, :controllers => { 
-    registrations: 'users/registrations', 
+  devise_for :users, :controllers => {
+    registrations: 'users/registrations',
     confirmations: 'users/confirmations'
   }
 
